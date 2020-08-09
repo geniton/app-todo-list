bafc10a525147c7263e1078d7b2de4b8442b3acc
Geniton Lima <geniton.lima@visie.com.br>
2020-08-09 08:52:07 -0300

    Removing SearchCepContainer

--- src/components/search-cep/index.js
+++ src/components/search-cep/index.js
@@ -1,18 +1,51 @@
 import React, { PureComponent } from 'react';
-import SearchCep from './search-cep'
 import { connect } from 'react-redux'
 import { fetchAddress } from 'reducers/address/action-creators'
 
-const SearchCepContainer = ({address,handleSubmit}) => (
-   <SearchCep
-      {...address}
-      handleSubmit={handleSubmit}
-   />
+const searchCep = ({
+   address,
+   city,
+   code,
+   district,
+   state,
+   handleSubmit,
+   status,
+   isFetching
+}) => (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='cep' id='' />
+      <button type='submit' disabled={isFetching}>{isFetching ? 'Buscando...' : 'Buscar Endereço'}</button>
+    </form>
+
+    { status != 200 && <p>CEP não encontrado</p>}
+
+    { status === 200 &&
+    <table>
+      <thead>
+        <tr>
+          <td>CEP</td>
+          <td>Endereço</td>
+          <td>Bairro</td>
+          <td>Cidade</td>
+          <td>Estado</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{code}</td>
+          <td>{address}</td>
+          <td>{district}</td>
+          <td>{city}</td>
+          <td>{state}</td>
+        </tr>
+      </tbody>
+    </table>
+      }
+  </div>
 )
 
-const mapStateToProps = (state) => ({
-   address: state.address
-})
+const mapStateToProps = (state) => state.address
 
 const mapDispatchToProps = (dispatch) => {
    return {
@@ -25,4 +58,4 @@ const mapDispatchToProps = (dispatch) => {
 
 // const mapDispatchToProps = { updateAddress } Podemos usar o mapDispatachToProps assim tbm, como um objeto
 
-export default connect(mapStateToProps,mapDispatchToProps)(SearchCepContainer);
+export default connect(mapStateToProps,mapDispatchToProps)(searchCep);
